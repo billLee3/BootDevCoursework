ef8d45f7f4557fca322093c68b5ff3e7c2a627a5
billLee3 <79368180+billLee3@users.noreply.github.com>
2025-09-05 23:10:40 -0500

    Create insertionsort.py

---
+++ insertionsort.py
@@ -0,0 +1,9 @@
+def insertion_sort(nums):
+    nums_copy = nums.copy()
+    for i in range(0, len(nums_copy)):
+        j = i
+        while j > 0:
+            if nums_copy[(j - 1)] > nums_copy[j]:
+                nums_copy[j], nums_copy[j-1] = nums_copy[j-1], nums_copy[j]
+            j -= 1
+    return nums_copy
